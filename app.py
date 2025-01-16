de8517434f9f75bf5d1488d502ff7b585b21102d
Rohan Chatse <105277705+Chatserohan@users.noreply.github.com>
2025-01-16 21:08:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,10 +5,10 @@ app = Flask(__name__)
 
 # Load the trained model and vectorizer
 with open('model.pkl', 'rb') as model_file:
-    model = pickle.load(model_file)
+    model = pickle.load(model)
 
 with open('vectorizer.pkl', 'rb') as vectorizer_file:
-    vectorizer = pickle.load(vectorizer_file)
+    vectorizer = pickle.load(vectorizer)
 
 # Home route to display the form
 @app.route('/')
